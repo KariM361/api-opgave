69df95e890f910654ed45f90665104374a00148a
Kari idskov holm Madsen <kama7@elev.techcollege.dk>
2025-09-30 09:47:08 +0200

    dogside

--- Js/script.js
+++ Js/script.js
@@ -1,1 +1,18 @@
-fetch('https://dog.ceo/dog-api/')
+fetch('https://dog.ceo/api/breeds/image/random/3')
+  .then(response => response.json())
+  .then(data => {
+    view(data);
+  })
+  .catch(error => {
+    console.error('Fejl:', error);
+  });
+
+function view(data) {
+  const output = document.getElementById('output');
+  // Lav HTML for billederne
+  const imagesHtml = data.message.map(url => `<img src="${url}" alt="Dog" style="max-width:200px;">`).join('');
+  output.innerHTML = `
+    <h2>Hundebilleder</h2>
+    ${imagesHtml}
+  `;
+}
